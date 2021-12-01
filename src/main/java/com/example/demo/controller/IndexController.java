2990af0ea0c06206b696f7207ae7da781fa61d8a
AntonioJRM1998 <antoniojoserodriguez1998@gmail.com>
2021-12-01 17:23:09 +0100

    Actualizacion de servicio

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,27 +22,27 @@ public class IndexController {
 	private Counter counterFar;
 	private final static Logger logger= LoggerFactory.getLogger(IndexController.class);
 	
-	public IndexController(MeterRegistry meterRegistry,MeterRegistry meterRegistry2) {
+	public IndexController(MeterRegistry meterRegistry) {
 		this.counterGrados=Counter.builder("Invocaciones mas usadas").description("Total de invocaciones para Grados").register(meterRegistry);
 		this.counterFar=Counter.builder("Invocaciones mas usadas").description("Total de invocaciones para Fahrenheit").register(meterRegistry);
+	}
+	@GetMapping("/tipo/{temp}")
+	public ResponseEntity<String> index(@PathVariable int temp) {
+		logger.info("Mostrando un tipo de temperatura");
+		if(temperatura.tipoTemp(temp)>temp) {
+			counterFar.increment();
+			return new ResponseEntity<String>(HttpStatus.OK).ok("Grados Celcius "+temperatura.tipoTemp(temp));
+		}else{
+			counterGrados.increment();
+			return new ResponseEntity<String>(HttpStatus.OK).ok("Grados Fahrenheit "+temperatura.tipoTemp(temp));
+		}
 	}
 	@Value("${some.value}")
 	private String myValue;
 	
 	@GetMapping(path="/myValue")
 	public String myValue() {
-		System.out.print(myValue);
 		return this.temperatura.mostrarTemperaturas(Integer.parseInt(myValue));
 	}
-	@GetMapping("/tipo/{temp}")
-	public String tipoTemp(@PathVariable int temp ) {
-		logger.info("Mostrando un tipo de temperatura");
-		if(temperatura.tipoTemp(temp)>temp) {
-			counterFar.increment();
-			return "Grados Fahrenheit "+temperatura.tipoTemp(temp);
-		}else{
-			counterGrados.increment();
-			return "Grados Celcius "+temperatura.tipoTemp(temp);
-		}
-	}
+
 }
